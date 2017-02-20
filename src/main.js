ac545c37217118ef1ed4387e4f51e0dee9c4f3dc
Jérém <jeremzor@gmail.com>
2017-02-20 17:51:38 +0100

    feat: blur while moving mouse 🤓

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ let height = 369
 
 let startPoint
 let endPoint
+let theMask
+let carBlurred
 // ----------------------------------------------------------------------------------------------------
 // Create the renderer
 // ----------------------------------------------------------------------------------------------------
@@ -37,38 +39,70 @@ function setup () {
     PIXI.loader.resources['./static/car.png'].texture
   )
   stage.addChild(car)
-
   stage.interactive = true
-  stage.on('mousedown', handleDown)
-  stage.on('mouseup', handleUp)
 
-  // render
-  renderer.render(stage)
-}
-
-function handleDown (event) {
-  startPoint = new PIXI.Point(event.data.global.x, event.data.global.y)
-}
-
-function handleUp (event) {
-  endPoint = new PIXI.Point(event.data.global.x, event.data.global.y)
-
-  let carBlurred = new PIXI.Sprite(
+  carBlurred = new PIXI.Sprite(
     PIXI.loader.resources['./static/car.png'].texture
   )
 
   var blurFilter1 = new PIXI.filters.BlurFilter(3, 3)
   carBlurred.filters = [blurFilter1]
-
-  // mask
-  let theMask = new PIXI.Graphics()
-  theMask.beginFill()
-  theMask.drawRect(startPoint.x, startPoint.y, ((endPoint.x - startPoint.x) + 20), ((endPoint.y - startPoint.y) + 20))
-  theMask.endFill()
-  stage.addChild(theMask)
-  carBlurred.mask = theMask
   stage.addChild(carBlurred)
 
-  console.log(startPoint, endPoint)
+  theMask = new PIXI.Graphics()
+  theMask.beginFill()
+  theMask.drawRect(0, 0, 1, 1)
+  theMask.endFill()
+  carBlurred.mask = theMask
+
+  stage.addChild(theMask)
+  stage.on('mousedown', handleDown)
+  stage.on('mouseup', handleUp)
+  // render
+  // renderer.render(stage)
+  window.requestAnimationFrame(render)
+}
+
+// startMove
+function handleDown (event) {
+  console.log('down:', event)
+  startPoint = new PIXI.Point(event.data.global.x, event.data.global.y)
+  stage.on('mousemove', handleMove)
+  theMask.beginFill()
+}
+
+function handleMove (event) {
+  endPoint = new PIXI.Point(event.data.global.x, event.data.global.y)
+  theMask.clear()
+  theMask.drawRect(startPoint.x, startPoint.y, ((endPoint.x - startPoint.x) + 20), ((endPoint.y - startPoint.y) + 20))
+}
+
+function handleUp (event) {
+  // endPoint = new PIXI.Point(event.data.global.x, event.data.global.y)
+  // theMask.drawRect(startPoint.x, startPoint.y, ((endPoint.x - startPoint.x) + 20), ((endPoint.y - startPoint.y) + 20))
+  theMask.endFill()
+  stage.off('mousemove', handleMove)
+  // let carBlurred = new PIXI.Sprite(
+  //   PIXI.loader.resources['./static/car.png'].texture
+  // )
+  //
+  // var blurFilter1 = new PIXI.filters.BlurFilter(3, 3)
+  // carBlurred.filters = [blurFilter1]
+  //
+  // // mask
+  // let theMask = new PIXI.Graphics()
+  // theMask.beginFill()
+  // theMask.drawRect(startPoint.x, startPoint.y, ((endPoint.x - startPoint.x) + 20), ((endPoint.y - startPoint.y) + 20))
+  // theMask.endFill()
+  // stage.addChild(theMask)
+  // carBlurred.mask = theMask
+  // stage.addChild(carBlurred)
+  //
+  // console.log(startPoint, endPoint)
+  // renderer.render(stage)
+}
+
+function render () {
   renderer.render(stage)
+  window.requestAnimationFrame(render)
 }
